db95fc08c4ada23c286e37e4413175313b0d3325
denmraq <bvg227fwk4@privaterelay.appleid.com>
2026-08-31 22:17:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'eth-radar-v4';
+const CACHE = 'eth-radar-v5';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
